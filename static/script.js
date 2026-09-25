fed64e47491e71de4b7c70b4062ffb549192602a
Minu Thillai Ganapathy <minu.ganapathy.07@gmail.com>
2026-09-25 07:30:12 +0530

    First commit

---
+++ static/script.js
@@ -0,0 +1,16 @@
+const marketSelect = document.getElementById("market");
+const assessButton = document.getElementById("assess-button");
+
+assessButton.addEventListener("click", function () {
+
+    const selectedMarket = marketSelect.value;
+
+    if (!selectedMarket) {
+        alert("Please select a market first.");
+        return;
+    }
+
+    alert("Assessment for " + marketSelect.options[marketSelect.selectedIndex].text + " will begin.");
+});
+
+console.log("JavaScript is connected!");
